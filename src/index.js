bb769779a4ce43862b56c8fa86253e916df14591
Marcelo Vedroni <marcelo.vedroni@zenvia.com>
2021-04-29 11:38:05 -0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { request, response } = require("express");
+const { request, response, json } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
@@ -7,66 +7,48 @@ app.use(express.json())
 
 const customers = [];
 
-app.get("/", (request, response) => {
-  
-  response.json(customers);
-})
-
-function verifyIfExistsAccountCPF(request, response, next) {
+function verifyExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers;
 
- const customer = customers.find(customer => customer.cpf === cpf);
+  const customer = customers.find(customer => customer.cpf === cpf);
 
- if(!customer) {
-   return response.status(401).json({ error: "Customer not found"});
- }
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found"});
+  }
 
- request.customer = customer;
+  request.customer = customer;
 
- return next();
+  return next();
 }
 
+app.get("/", (request, response) => {
+  
+  return response.json(customers);
+})
+
 app.post("/account", (request, response) => {
-  const { name, cpf } = request.body;
+  const { cpf, name } = request.body;
 
-  const cpfAlreadyExist = customers.some(customer => customer.cpf === cpf);
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
 
-  if(cpfAlreadyExist) {
-    return response.status(400).json({error: "Already exists"});
+  if(customerAlreadyExists) {
+    return response.status(400).json({error: 'Customer already exists'});
   }
 
   customers.push({
-    name,
     cpf,
+    name,
     id: uuidv4(),
     statement: []
-  }); 
-
-  return response.status(200).json();
-})
-
-
-app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
-  const { customer } = request;
-
- return response.json(customer.statement);
-})
-
-app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
-  const { description, amount } = request.body;
-
-  const { customer } = request;
-
-  const statementOperation = {
-    description,
-    amount,
-    created_at: new Date(),
-    type: "credit",
-  }
-
-  customer.statement.push(statementOperation);
+  });
 
   return response.status(201).json();
 })
+
+app.get("/statement", verifyExistsAccountCPF, (request, response) => {  
+  const { customer } = request;
+  
+  return response.json(customer.statement);
+});
 
 app.listen(3333);
