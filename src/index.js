cdfab9cd03c5bad96e79582426833cb3b0c6a81c
Marcelo Vedroni <marcelo.vedroni@zenvia.com>
2021-04-29 23:12:18 -0300

    Withdraw operation

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ function verifyExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.get("/", (request, response) => {
   
   return response.json(customers);
@@ -60,12 +72,34 @@ app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
     description,
     amount,
     created_at: new Date(),
-    type: "Credit"
+    type: "credit"
   }
 
   customer.statement.push(statementOperation);
 
   return response.status(201).json(statementOperation);
+})
+
+app.post("/withdraw", verifyExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Not enough funds"});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperation);
+
+
+  return response.status(201).send();
 })
 
 app.listen(3333);
